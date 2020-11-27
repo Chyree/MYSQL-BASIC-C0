68b42d71ad31aedced44d8f4141b2482b991e78b
= <=>
2020-11-27 06:55:08 +0100

    s

--- 02-Select/taak03-AND-OR/antwoorden.sql
+++ 02-Select/taak03-AND-OR/antwoorden.sql
@@ -1,8 +1,38 @@
 -- Opdracht 1 
+127.0.0.1/mod-mysql-basic-fifa2018/players/		http://localhost/phpmyadmin/db_sql.php?db=mod-mysql-basic-fifa2018
+ Showing rows 0 -  5 (6 total, Query took 0.0026 seconds.)
+
+SELECT * FROM players WHERE nationality = "Spain" AND club="Chelsea"
+
+
+id	name	age	nationality	club	value	wage	
+162895	Cesc F bregas 	30	Spain 	Chelsea 	41000000	210000	
+179844	Diego Costa 	28	Spain 	Chelsea 	46000000	235000	
+184432	Azpilicueta 	27	Spain 	Chelsea 	375000000	160000	
+189505	Pedro 	29	Spain 	Chelsea 	305000000	200000	
+192638	Marcos Alonso 	26	Spain 	Chelsea 	175000000	115000	
+201153	Morata 	24	Spain 	Chelsea 	41000000	170000	
 
 -- Opdracht 2 
+127.0.0.1/mod-mysql-basic-fifa2018/players/		http://localhost/phpmyadmin/tbl_sql.php?db=mod-mysql-basic-fifa2018&table=players
+ Showing rows 0 -  0 (1 total, Query took 0.0026 seconds.)
+
+SELECT * FROM players WHERE nationality = "Spain" AND age = 17 AND wage = 15000
+
+
+
+231410	Brahim D az 	17	Spain 	Manchester City 	13000000	15000	
 
 -- Opdracht 3
+127.0.0.1/mod-mysql-basic-fifa2018/players/		http://localhost/phpmyadmin/tbl_sql.php?db=mod-mysql-basic-fifa2018&table=players
+ Showing rows 0 -  1 (2 total, Query took 0.0026 seconds.)
+
+SELECT * FROM players WHERE club="liverpool" AND age=20
+
+
+id	name	age	nationality	club	value	wage	
+220710	H. Wilson 	20	Wales 	Liverpool 	550000	13000	
+225100	J. Gomez 	20	England 	Liverpool 	28000000	40000	
 
 -- Opdracht 4
 
